057b92450d2352e2b6fa01107b202c68b728cbf9
PeryShahar <pery1shahar@gmail.com>
2023-11-06 14:42:17 +0200

    feat: upload images & grid layout

---
+++ data/recipes.ts
@@ -0,0 +1,128 @@
+import veganChraime from '../assets/vegan-chraime.jpg'
+import peaSoup from '../assets/pea-soup.jpg'
+import chickpeaPatties from '../assets/chickpea-patties.jpg'
+import energyBalls from '../assets/energy-balls.jpg'
+import mexicanMix from '../assets/mexican-mix.jpg'
+import cholent from '../assets/cholent.jpg'
+import fudgyBrownies from '../assets/fudgy-brownies.png'
+import blackLentilsHummus from '../assets/black-lentils-hummus.jpg'
+import shreddedTofu from '../assets/shredded-tofu.png'
+import greekFava from '../assets/greek-fava.jpg'
+import blackLentilsStew from '../assets/black-lentils-stew.jpg'
+import dalGreenLentils from '../assets/dal-green-lentils.jpg'
+import roastedCauliflower from '../assets/roasted-cauliflower.jpg'
+
+export  const recipes = [
+{
+    "id": 1,
+    "title": "Vegan Chraime",
+    "category": ["Lunch"],
+    "link": "https://veg.co.il/recipes/patties/vegan-chraime/",
+    "img": veganChraime
+},
+{
+    "id": 2,
+    "title":"Pea Soup",
+    "category": ["Soups", "Lunch"],
+    "link": "https://veg.co.il/recipes/soup/pea-soup/",
+    "img": peaSoup
+},
+{
+    "id": 3,
+    "title":"Chickpea patties",
+    "category": ["Patties", "Lunch"],
+    "link": "https://veg.co.il/recipes/patties/chickpea-patties/",
+    "img": chickpeaPatties
+},
+{
+    "id": 4,
+    "title":"Energy Balls",
+    "category": ["Desert"],
+    "link": "https://veg.co.il/recipes/desserts/energy-balls/",
+    "img": energyBalls
+},
+{
+    "id": 5,
+    "title":"Mexican Mix",
+    "category": ["Lunch"],
+    "link": "https://www.windowlight.co.il/%D7%AA%D7%91%D7%A9%D7%99%D7%9C-%D7%9E%D7%A7%D7%A1%D7%99%D7%A7%D7%A0%D7%99-%D7%91%D7%A1%D7%99%D7%A8-%D7%90%D7%97%D7%93/",
+    "img": mexicanMix
+},
+{
+    "id": 6,
+    "title":"Veg Cholent (Hamin)",
+    "category": ["Lunch"],
+    "link": "https://veg.co.il/recipes/stews/cholent/",
+    "img": cholent
+},
+{
+    "id": 7,
+    "title":"Fudgy Brownies",
+    "category": ["Desert"],
+    "link": "https://www.instagram.com/reel/CmUzfmiIHuF/?igshid=MDJmNzVkMjY%3D",
+    "img": fudgyBrownies
+},
+{
+    "id": 8,
+    "title":"Black Lentils Hummus",
+    "category": ["Lunch", "Spreads"],
+    "link": "https://www.mako.co.il/food-cooking_magazine/healthy-eating-recipes/Recipe-f6417af2085d651006.htm",
+    "img": blackLentilsHummus
+},
+{
+    "id": 9,
+    "title":"Shredded Tofu",
+    "category": ["Lunch"],
+    "link": "https://www.instagram.com/reel/CnSb3Q_KHXz/?igshid=MDJmNzVkMjY%3D",
+    "img": shreddedTofu
+},
+{
+    "id": 10,
+    "title":"Greek Fava",
+    "category": ["Lunch","Spreads"],
+    "link": "https://www.gimmesomeoven.com/greek-fava/",
+    "img": greekFava
+},
+{
+    "id": 11,
+    "title":"Black Lentils Stew",
+    "category": ["Lunch"],
+    "link": "https://tivoneat.co.il/%D7%AA%D7%91%D7%A9%D7%99%D7%9C-%D7%A2%D7%93%D7%A9%D7%99%D7%9D-%D7%A9%D7%97%D7%95%D7%A8%D7%95%D7%AA-%D7%98%D7%91%D7%A2%D7%95%D7%A0%D7%99/",
+    "img": blackLentilsStew
+},
+{
+    "id": 12,
+    "title":"Dal Green Lentils",
+    "category": ["Lunch"],
+    "link": "https://pormeleg.kitchen/vegan-green-lentil-dahl/",
+    "img": dalGreenLentils
+},
+{
+    "id": 13,
+    "title":"Roasted Cauliflower",
+    "category": ["Lunch"],
+    "link": "https://recipe30.com/roasted-cauliflower-florets.html/",
+    "img": roastedCauliflower
+},
+{
+    "id": 14,
+    "title":"",
+    "category": ["Lunch"],
+    "link": "",
+    "img": ""
+},
+{
+    "id": 15,
+    "title":"",
+    "category": [],
+    "link": "",
+    "img": ""
+},
+{
+    "id": 16,
+    "title":"",
+    "category": [],
+    "link": "",
+    "img": ""
+}
+]
