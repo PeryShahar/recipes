ea1f6ae5ddc304c01d8d049b850d480f23584282
PeryShahar <pery1shahar@gmail.com>
2024-02-25 18:00:01 +0200

    feat: add new recipe -> apple pie

--- app/lib/recipes.ts
+++ app/lib/recipes.ts
@@ -1,203 +1,210 @@
 
-export  const recipes = [
-{
-    "id": 1,
-    "title": "Vegan Chraime",
-    "category": ["Lunch"],
-    "link": "https://veg.co.il/recipes/patties/vegan-chraime/",
-    "img": '/recipes/vegan-chraime.jpg'
-},
-{
-    "id": 2,
-    "title":"Pea Soup",
-    "category": ["Soups", "Lunch"],
-    "link": "https://veg.co.il/recipes/soup/pea-soup/",
-    "img": '/recipes/pea-soup.jpg'
-},
-{
-    "id": 3,
-    "title":"Chickpea patties",
-    "category": [ "Lunch"],
-    "link": "https://veg.co.il/recipes/patties/chickpea-patties/",
-    "img": '/recipes/chickpea-patties.jpg'
-},
-{
-    "id": 4,
-    "title":"Energy Balls",
-    "category": ["Dessert"],
-    "link": "https://veg.co.il/recipes/desserts/energy-balls/",
-    "img": '/recipes/energy-balls.jpg'
-},
-{
-    "id": 5,
-    "title":"Mexican Mix",
-    "category": ["Lunch"],
-    "link": "https://www.windowlight.co.il/%D7%AA%D7%91%D7%A9%D7%99%D7%9C-%D7%9E%D7%A7%D7%A1%D7%99%D7%A7%D7%A0%D7%99-%D7%91%D7%A1%D7%99%D7%A8-%D7%90%D7%97%D7%93/",
-    "img": '/recipes/mexican-mix.jpg'
-},
-{
-    "id": 6,
-    "title":"Veg Cholent (Hamin)",
-    "category": ["Lunch"],
-    "link": "https://veg.co.il/recipes/stews/cholent/",
-    "img": '/recipes/cholent.jpg'
-},
-{
-    "id": 7,
-    "title":"Fudgy Brownies",
-    "category": ["Dessert"],
-    "link": "https://www.instagram.com/reel/CmUzfmiIHuF/?igshid=MDJmNzVkMjY%3D",
-    "img": '/recipes/fudgy-brownies.png'
-},
-{
-    "id": 8,
-    "title":"Black Lentils Hummus",
-    "category": ["Lunch", "Spreads"],
-    "link": "https://www.mako.co.il/food-cooking_magazine/healthy-eating-recipes/Recipe-f6417af2085d651006.htm",
-    "img": '/recipes/black-lentils-hummus.jpg'
-},
-{
-    "id": 9,
-    "title":"Shredded Tofu",
-    "category": ["Lunch"],
-    "link": "https://www.instagram.com/reel/CnSb3Q_KHXz/?igshid=MDJmNzVkMjY%3D",
-    "img": '/recipes/shredded-tofu.png'
-},
-{
-    "id": 10,
-    "title":"Greek Fava",
-    "category": ["Lunch","Spreads"],
-    "link": "https://www.gimmesomeoven.com/greek-fava/",
-    "img": '/recipes/greek-fava.jpg'
-},
-{
-    "id": 11,
-    "title":"Black Lentils Stew",
-    "category": ["Lunch"],
-    "link": "https://tivoneat.co.il/%D7%AA%D7%91%D7%A9%D7%99%D7%9C-%D7%A2%D7%93%D7%A9%D7%99%D7%9D-%D7%A9%D7%97%D7%95%D7%A8%D7%95%D7%AA-%D7%98%D7%91%D7%A2%D7%95%D7%A0%D7%99/",
-    "img": '/recipes/black-lentils-stew.jpg'
-},
-{
-    "id": 12,
-    "title":"Dal Green Lentils",
-    "category": ["Lunch"],
-    "link": "https://pormeleg.kitchen/vegan-green-lentil-dahl/",
-    "img": '/recipes/dal-green-lentils.jpg'
-},
-{
-    "id": 13,
-    "title":"Roasted Cauliflower",
-    "category": ["Lunch"],
-    "link": "https://recipe30.com/roasted-cauliflower-florets.html/",
-    "img": '/recipes/roasted-cauliflower.jpg'
-},
-{
-    "id": 14,
-    "title":"Fermented Cashew Cheese",
-    "category": ["Lunch"],
-    "link": "https://rainbowplantlife.com/fermented-cashew-cheese/",
-    "img": '/recipes/fermented-cashew-cheese.jpg'
-},
-{
-    "id": 15,
-    "title":"Yellow Split Pea Dal",
-    "category": ["Lunch"],
-    "link": "https://plantbasedfolk.com/yellow-split-pea-dal-recipe/",
-    "img": '/recipes/yellow-split-pea-dahl.jpg'
-},
-{
-    "id": 16,
-    "title":"Vegan Curry with Tofu",
-    "category": ["Lunch"],
-    "link": "https://rainbowplantlife.com/vegan-curry-with-tofu/",
-    "img": '/recipes/vegan-curry-with-tofu.jpg'
-},
-{
-    "id": 17,
-    "title":"Chickpea Omelette",
-    "category": ["Breakfast"],
-    "link": "https://www.theconsciousplantkitchen.com/chickpea-omelette/#Vegan_Chickpea_Omelette",
-    "img": '/recipes/chickpea-omelette.jpg'
-},
-{
-    "id": 18,
-    "title":"Vegan Palak Panner",
-    "category": ["Lunch"],
-    "link": "https://rainbowplantlife.com/vegan-palak-paneer-with-tofu/",
-    "img": '/recipes/palak-panner-with-tofu.jpg'
-},
-{
-    "id": 19,
-    "title":"Seaweeds Rice",
-    "category": ["Lunch"],
-    "link": "https://www.instagram.com/reel/CopicbCql9h/?igshid=NjZiM2M3MzIxNA%3D%3D",
-    "img": '/recipes/seaweeds-rice.png'
-},
-{
-    "id": 20,
-    "title":"Crispy Sesame Crusted Tofu",
-    "category": ["Lunch"],
-    "link": "https://sarahsvegankitchen.com/recipes/crispy-sesame-crusted-tofu/",
-    "img": '/recipes/crispy-sesame-tofu.jpeg'
-},
-{
-    "id": 21,
-    "title":"Veggie Burger",
-    "category": ["Lunch"],
-    "link": "https://www.inspiredtaste.net/36554/veggie-burger-recipe/",
-    "img": '/recipes/veggie-burger.jpg'
-},
-{
-    "id": 22,
-    "title":"Almond Butter",
-    "category": ["Spreads"],
-    "link": "https://downshiftology.com/how-to-make-homemade-almond-butter/",
-    "img": '/recipes/almond-butter.jpg'
-},
-{
-    "id": 23,
-    "title":"Vegan Malabi",
-    "category": ["Dessert"],
-    "link": "https://tivoneat.co.il/vegan-gf-malabi/",
-    "img": '/recipes/vegan-malabi.jpg'
-},
-{
-    "id": 24,
-    "title":"Simple Quinoa",
-    "category": ["Lunch"],
-    "link": "https://www.supergood.co.il/%D7%90%D7%99%D7%9A-%D7%9C%D7%94%D7%9B%D7%99%D7%9F-%D7%A7%D7%99%D7%A0%D7%95%D7%90%D7%94-%D7%90%D7%97%D7%AA-%D7%90%D7%97%D7%AA/",
-    "img": '/recipes/simple-quinoa.jpg'
-},
-{
-    "id": 25,
-    "title":"Green Peas Curry",
-    "category": ["Lunch"],
-    "link": "https://www.flavourstreat.com/green-peas-masala-matar-curry/",
-    "img": '/recipes/green-peas-curry.jpg'
-},{
-    "id": 26,
-    "title":"Turmeric Golden Milk",
-    "category": ["Beverages"],
-    "link": "https://rainbowplantlife.com/vegan-golden-milk-turmeric-latte/",
-    "img": '/recipes/turmeric-golden-tea.jpg'
-},{
-    "id": 27,
-    "title":"Mexican Black Beans",
-    "category": ["Lunch"],
-    "link": "https://belleofthekitchen.com/mexican-black-beans-recipe/",
-    "img": '/recipes/mexican-black-beans.jpg'
-},{
-    "id": 28,
-    "title":"Oven Roasted Vegetables",
-    "category": ["Lunch"],
-    "link": "https://www.wellplated.com/oven-roasted-vegetables/#wprm-recipe-container-35156",
-    "img": '/recipes/roasted-vegetables-oven.jpeg'
-},
-{
-    "id": 29,
-    "title":"Pesto",
-    "category": ["Spreads"],
-    "link": "https://rainbowplantlife.com/how-to-make-vegan-pesto/",
-    "img": '/recipes/pesto.jpeg'
-},
+export const recipes = [
+    {
+        "id": 1,
+        "title": "Vegan Chraime",
+        "category": ["Lunch"],
+        "link": "https://veg.co.il/recipes/patties/vegan-chraime/",
+        "img": '/recipes/vegan-chraime.jpg'
+    },
+    {
+        "id": 2,
+        "title": "Pea Soup",
+        "category": ["Soups", "Lunch"],
+        "link": "https://veg.co.il/recipes/soup/pea-soup/",
+        "img": '/recipes/pea-soup.jpg'
+    },
+    {
+        "id": 3,
+        "title": "Chickpea patties",
+        "category": ["Lunch"],
+        "link": "https://veg.co.il/recipes/patties/chickpea-patties/",
+        "img": '/recipes/chickpea-patties.jpg'
+    },
+    {
+        "id": 4,
+        "title": "Energy Balls",
+        "category": ["Dessert"],
+        "link": "https://veg.co.il/recipes/desserts/energy-balls/",
+        "img": '/recipes/energy-balls.jpg'
+    },
+    {
+        "id": 5,
+        "title": "Mexican Mix",
+        "category": ["Lunch"],
+        "link": "https://www.windowlight.co.il/%D7%AA%D7%91%D7%A9%D7%99%D7%9C-%D7%9E%D7%A7%D7%A1%D7%99%D7%A7%D7%A0%D7%99-%D7%91%D7%A1%D7%99%D7%A8-%D7%90%D7%97%D7%93/",
+        "img": '/recipes/mexican-mix.jpg'
+    },
+    {
+        "id": 6,
+        "title": "Veg Cholent (Hamin)",
+        "category": ["Lunch"],
+        "link": "https://veg.co.il/recipes/stews/cholent/",
+        "img": '/recipes/cholent.jpg'
+    },
+    {
+        "id": 7,
+        "title": "Fudgy Brownies",
+        "category": ["Dessert"],
+        "link": "https://www.instagram.com/reel/CmUzfmiIHuF/?igshid=MDJmNzVkMjY%3D",
+        "img": '/recipes/fudgy-brownies.png'
+    },
+    {
+        "id": 8,
+        "title": "Black Lentils Hummus",
+        "category": ["Lunch", "Spreads"],
+        "link": "https://www.mako.co.il/food-cooking_magazine/healthy-eating-recipes/Recipe-f6417af2085d651006.htm",
+        "img": '/recipes/black-lentils-hummus.jpg'
+    },
+    {
+        "id": 9,
+        "title": "Shredded Tofu",
+        "category": ["Lunch"],
+        "link": "https://www.instagram.com/reel/CnSb3Q_KHXz/?igshid=MDJmNzVkMjY%3D",
+        "img": '/recipes/shredded-tofu.png'
+    },
+    {
+        "id": 10,
+        "title": "Greek Fava",
+        "category": ["Lunch", "Spreads"],
+        "link": "https://www.gimmesomeoven.com/greek-fava/",
+        "img": '/recipes/greek-fava.jpg'
+    },
+    {
+        "id": 11,
+        "title": "Black Lentils Stew",
+        "category": ["Lunch"],
+        "link": "https://tivoneat.co.il/%D7%AA%D7%91%D7%A9%D7%99%D7%9C-%D7%A2%D7%93%D7%A9%D7%99%D7%9D-%D7%A9%D7%97%D7%95%D7%A8%D7%95%D7%AA-%D7%98%D7%91%D7%A2%D7%95%D7%A0%D7%99/",
+        "img": '/recipes/black-lentils-stew.jpg'
+    },
+    {
+        "id": 12,
+        "title": "Dal Green Lentils",
+        "category": ["Lunch"],
+        "link": "https://pormeleg.kitchen/vegan-green-lentil-dahl/",
+        "img": '/recipes/dal-green-lentils.jpg'
+    },
+    {
+        "id": 13,
+        "title": "Roasted Cauliflower",
+        "category": ["Lunch"],
+        "link": "https://recipe30.com/roasted-cauliflower-florets.html/",
+        "img": '/recipes/roasted-cauliflower.jpg'
+    },
+    {
+        "id": 14,
+        "title": "Fermented Cashew Cheese",
+        "category": ["Lunch"],
+        "link": "https://rainbowplantlife.com/fermented-cashew-cheese/",
+        "img": '/recipes/fermented-cashew-cheese.jpg'
+    },
+    {
+        "id": 15,
+        "title": "Yellow Split Pea Dal",
+        "category": ["Lunch"],
+        "link": "https://plantbasedfolk.com/yellow-split-pea-dal-recipe/",
+        "img": '/recipes/yellow-split-pea-dahl.jpg'
+    },
+    {
+        "id": 16,
+        "title": "Vegan Curry with Tofu",
+        "category": ["Lunch"],
+        "link": "https://rainbowplantlife.com/vegan-curry-with-tofu/",
+        "img": '/recipes/vegan-curry-with-tofu.jpg'
+    },
+    {
+        "id": 17,
+        "title": "Chickpea Omelette",
+        "category": ["Breakfast"],
+        "link": "https://www.theconsciousplantkitchen.com/chickpea-omelette/#Vegan_Chickpea_Omelette",
+        "img": '/recipes/chickpea-omelette.jpg'
+    },
+    {
+        "id": 18,
+        "title": "Vegan Palak Panner",
+        "category": ["Lunch"],
+        "link": "https://rainbowplantlife.com/vegan-palak-paneer-with-tofu/",
+        "img": '/recipes/palak-panner-with-tofu.jpg'
+    },
+    {
+        "id": 19,
+        "title": "Seaweeds Rice",
+        "category": ["Lunch"],
+        "link": "https://www.instagram.com/reel/CopicbCql9h/?igshid=NjZiM2M3MzIxNA%3D%3D",
+        "img": '/recipes/seaweeds-rice.png'
+    },
+    {
+        "id": 20,
+        "title": "Crispy Sesame Crusted Tofu",
+        "category": ["Lunch"],
+        "link": "https://sarahsvegankitchen.com/recipes/crispy-sesame-crusted-tofu/",
+        "img": '/recipes/crispy-sesame-tofu.jpeg'
+    },
+    {
+        "id": 21,
+        "title": "Veggie Burger",
+        "category": ["Lunch"],
+        "link": "https://www.inspiredtaste.net/36554/veggie-burger-recipe/",
+        "img": '/recipes/veggie-burger.jpg'
+    },
+    {
+        "id": 22,
+        "title": "Almond Butter",
+        "category": ["Spreads"],
+        "link": "https://downshiftology.com/how-to-make-homemade-almond-butter/",
+        "img": '/recipes/almond-butter.jpg'
+    },
+    {
+        "id": 23,
+        "title": "Vegan Malabi",
+        "category": ["Dessert"],
+        "link": "https://tivoneat.co.il/vegan-gf-malabi/",
+        "img": '/recipes/vegan-malabi.jpg'
+    },
+    {
+        "id": 24,
+        "title": "Simple Quinoa",
+        "category": ["Lunch"],
+        "link": "https://www.supergood.co.il/%D7%90%D7%99%D7%9A-%D7%9C%D7%94%D7%9B%D7%99%D7%9F-%D7%A7%D7%99%D7%A0%D7%95%D7%90%D7%94-%D7%90%D7%97%D7%AA-%D7%90%D7%97%D7%AA/",
+        "img": '/recipes/simple-quinoa.jpg'
+    },
+    {
+        "id": 25,
+        "title": "Green Peas Curry",
+        "category": ["Lunch"],
+        "link": "https://www.flavourstreat.com/green-peas-masala-matar-curry/",
+        "img": '/recipes/green-peas-curry.jpg'
+    }, {
+        "id": 26,
+        "title": "Turmeric Golden Milk",
+        "category": ["Beverages"],
+        "link": "https://rainbowplantlife.com/vegan-golden-milk-turmeric-latte/",
+        "img": '/recipes/turmeric-golden-tea.jpg'
+    }, {
+        "id": 27,
+        "title": "Mexican Black Beans",
+        "category": ["Lunch"],
+        "link": "https://belleofthekitchen.com/mexican-black-beans-recipe/",
+        "img": '/recipes/mexican-black-beans.jpg'
+    }, {
+        "id": 28,
+        "title": "Oven Roasted Vegetables",
+        "category": ["Lunch"],
+        "link": "https://www.wellplated.com/oven-roasted-vegetables/#wprm-recipe-container-35156",
+        "img": '/recipes/roasted-vegetables-oven.jpeg'
+    },
+    {
+        "id": 29,
+        "title": "Pesto",
+        "category": ["Spreads"],
+        "link": "https://rainbowplantlife.com/how-to-make-vegan-pesto/",
+        "img": '/recipes/pesto.jpeg'
+    },
+    {
+        "id": 30,
+        "title": "Apple Pie",
+        "category": ["Dessert"],
+        "link": "https://www.haaretz.co.il/food/sweets/vegan-desserts/2023-03-22/ty-article-magazine/00000186-ff2d-d31e-a9ef-ff6f93110000",
+        "img": '/recipes/apple-pie.png'
+    },
 ]
